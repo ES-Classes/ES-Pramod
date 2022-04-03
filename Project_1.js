8d6cd8082c241610d1dc369810bf9135a3137cd0
Pramod Ghimire <vc76it16@vedascollege.edu.np>
2022-04-03 12:00:56 +0545

    yoooooo

--- Project_1.js
+++ Project_1.js
@@ -2,4 +2,13 @@ var a='pramod ';
 var b='sexy';
 var sum = a+b;
 console.log(sum);
-var h="manish";
+console.log(1/0);
+var number = 26826826854826486254872648264825862872n;
+console.log(number);
+
+let x =90;
+let y =90;
+let x1= x++;
+let y1= ++y;
+console.log(x,y,x1,y1);
+
